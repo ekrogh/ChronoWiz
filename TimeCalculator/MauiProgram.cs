d9a0caa17e9efed70bca6c397fe8da348f45b51c
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2023-10-13 21:17:22 +0200

    Builds w.o. errors

--- TimeCalculator/MauiProgram.cs
+++ TimeCalculator/MauiProgram.cs
@@ -3,35 +3,34 @@ using CommunityToolkit.Maui.Core;
 using CommunityToolkit.Maui.Storage;
 using Microsoft.Extensions.Logging;
 
-namespace TimeCalculator
+namespace TimeCalculator;
+
+public static class MauiProgram
 {
-	public static class MauiProgram
+	public static MauiApp CreateMauiApp()
 	{
-		public static MauiApp CreateMauiApp()
-		{
-			var builder = MauiApp.CreateBuilder();
-			builder
-				.UseMauiApp<App>()
-				// Initialize the .NET MAUI Community Toolkit by adding the below line of code
-				.UseMauiCommunityToolkit()
-				.UseMauiCommunityToolkitCore()
-				.ConfigureFonts(fonts =>
-				{
-					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-					fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-				}).ConfigureEssentials(essentials =>
-				{
-					essentials.UseVersionTracking();
-				});
-			;
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			// Initialize the .NET MAUI Community Toolkit by adding the below line of code
+			.UseMauiCommunityToolkit()
+			.UseMauiCommunityToolkitCore()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			}).ConfigureEssentials(essentials =>
+			{
+				essentials.UseVersionTracking();
+			});
+		;
 
 #if DEBUG
-			builder.Logging.AddDebug();
+		builder.Logging.AddDebug();
 #endif
 
-			builder.Services.AddSingleton<IFileSaver>(FileSaver.Default);
+		builder.Services.AddSingleton<IFileSaver>(FileSaver.Default);
 
-			return builder.Build();
-		}
+		return builder.Build();
 	}
 }
