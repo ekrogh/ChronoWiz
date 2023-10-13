bbafa06311a4fa7a1e4eed5a0785cd8220ecf929
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2023-10-13 23:44:18 +0200

    Builds but won't run !?

--- TimeCalculator/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/TimeCalculator.AssemblyInfo.cs
+++ TimeCalculator/obj/Debug/net7.0-windows10.0.19041.0/win10-x64/TimeCalculator.AssemblyInfo.cs
@@ -11,6 +11,10 @@
 using System;
 using System.Reflection;
 
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PackageName", "cf3821df-ba15-45a6-9c1b-ef5c5f831624")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.PublisherName", "User Name")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Name", "TimeCalculator")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("Microsoft.Maui.ApplicationModel.AppInfo.Version", "1.0.0.1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("TimeCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
