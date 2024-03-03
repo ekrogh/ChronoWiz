3fd94223c82e7b78bebc16aee3419f71fccabfef
Eigil <eigilkrogh@eksit.dk>
2024-03-03 15:31:09 +0100

    No Package

--- macPublish_Ad_Hoc_No_Package.bat
+++ macPublish_Ad_Hoc_No_Package.bat
@@ -1,3 +1,3 @@
 dotnet build -t:Clean
 dotnet clean
-dotnet publish ChronoWiz\ChronoWiz.csproj -f net8.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:CreatePackage=true -p:EnableCodeSigning=true  -p:CodesignKey="Apple Development: Eigil Krogh (M39X2ZPF3C)" -p:CodesignProvision="ChronoWiz Profile Development MAC" -p:CodesignEntitlements="Platforms\MacCatalyst\Entitlements.Release.plist" -p:UseHardenedRuntime=true -o "d:\Users\eigil\Downloads\"
+dotnet publish ChronoWiz\ChronoWiz.csproj -f net8.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:EnableCodeSigning=true  -p:CodesignKey="Apple Development: Eigil Krogh (M39X2ZPF3C)" -p:CodesignProvision="ChronoWiz Profile Development MAC" -p:CodesignEntitlements="Platforms\MacCatalyst\Entitlements.Release.plist" -p:UseHardenedRuntime=true
