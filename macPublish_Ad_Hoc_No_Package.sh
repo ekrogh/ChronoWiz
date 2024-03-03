e236f731681589912a126036ffe52e45d5cb4ec8
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2024-03-03 15:05:38 +0100

    No package

---
+++ macPublish_Ad_Hoc_No_Package.sh
@@ -0,0 +1,3 @@
+dotnet build -t:Clean
+dotnet clean
+dotnet publish ChronoWiz/ChronoWiz.csproj -f net8.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:CreatePackage=true -p:EnableCodeSigning=true  -p:CodesignKey="Apple Development: Eigil Krogh (M39X2ZPF3C)" -p:CodesignProvision="ChronoWiz Profile Development MAC" -p:CodesignEntitlements="Platforms/MacCatalyst/Entitlements.Release.plist" -p:UseHardenedRuntime=true -o "/Users/eks/Downloads/"
