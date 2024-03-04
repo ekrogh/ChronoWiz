f891feb0d5585b4bccd6d775ef5c7dfa3e6afc20
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2024-03-04 02:23:00 +0100

    new cert and profile.

--- macPublish_App_Store_No_Package.sh
+++ macPublish_App_Store_No_Package.sh
@@ -1,3 +1,3 @@
 dotnet build -t:Clean
 dotnet clean
-dotnet publish ChronoWiz/ChronoWiz.csproj -f net8.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:EnableCodeSigning=true -p:CodesignKey="Apple Distribution: Eigil Krogh (4657Q2Y6NH)" -p:CodesignProvision="ChronoWiz Profile App Store MAC" -p:CodesignEntitlements="Platforms/MacCatalyst/Entitlements.Release.plist"
+dotnet publish ChronoWiz/ChronoWiz.csproj -f net8.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:EnableCodeSigning=true -p:CodesignKey="Apple Distribution: Eigil Krogh (4657Q2Y6NH)" -p:CodesignProvision="ChronoWiz Mac Ap Store Connect MacCatalyst" -p:CodesignEntitlements="Platforms/MacCatalyst/Entitlements.Release.plist"
