c869b138860b3643e98b103220c1d23f6bb3a4a9
Eigil Sorensen <eks@eksit.dk>
2024-03-05 10:32:09 +0100

    Ups

--- iosPublish_Ad_Hoc.bat
+++ iosPublish_Ad_Hoc.bat
@@ -1,4 +1,4 @@
 dotnet build -t:Clean
 dotnet clean
-rm -rf /Users/eks/Downloads/ChronoWiz*.*
-dotnet publish -f net8.0-ios -c Release -p:ArchiveOnBuild=true -p:RuntimeIdentifier=ios-arm64 -p:CodesignKey="Apple Distribution: Eigil Krogh (4657Q2Y6NH)" -p:CodesignProvision="ChronoWiz Profile Ad Hoc iOS" -p:ServerAddress=skimac -p:ServerUser=eks -p:ServerPassword=sohbdk -p:TcpPort=58181 -p:_DotNetRootRemoteDirectory=/Users/eks/Library/Caches/Xamarin/XMA/SDKs/dotnet/  -o "/Users/eks/Downloads/"
+del D:\Users\eigil\Downloads\ChronoWiz*.*
+dotnet publish ChronoWiz/ChronoWiz.csproj -f net8.0-ios -c Release -p:ArchiveOnBuild=true -p:RuntimeIdentifier=ios-arm64 -p:CodesignKey="Apple Distribution: Eigil Krogh (4657Q2Y6NH)" -p:CodesignProvision="ChronoWiz Profile Ad Hoc iOS" -p:ServerAddress=skimac -p:ServerUser=eks -p:ServerPassword=sohbdk -p:TcpPort=58181 -p:_DotNetRootRemoteDirectory=/Users/eks/Library/Caches/Xamarin/XMA/SDKs/dotnet/  -o "D:\Users\eigil\Downloads"
