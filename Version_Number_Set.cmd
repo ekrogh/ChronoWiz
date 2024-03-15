fa01ad135b1332119e0ff088eeb2ffbff3ea3e70
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2024-03-15 21:42:43 +0100

    Ver.: 1.1.7.7.

--- Version_Number_Set.cmd
+++ Version_Number_Set.cmd
@@ -1,3 +1,3 @@
-D:\Program_Files\Microsoft_Visual_Studio\2022\Community\Common7\IDE\devenv.exe .\ChronoMate.sln
-@REM call Code.cmd .\snap\snapcraft.yaml .\snap\gui\ChronoMate.desktop
+D:\Program_Files\Microsoft_Visual_Studio\2022\Community\Common7\IDE\devenv.exe .\ChronoWiz.sln
+@REM call Code.cmd .\snap\snapcraft.yaml .\snap\gui\ChronoWiz.desktop
 D:\Program_Files\SmartGit\bin\smartgit.exe .
