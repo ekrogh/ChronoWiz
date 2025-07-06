77607f940d789536ba9d1bfd65e27be0eafbe3f2
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2025-07-06 04:39:57 +0200

    Committing pending changes before .NET 9.0 upgrade

---
+++ ChronoWizCore/ICSFiling/FileICS.xaml.cs
@@ -0,0 +1,55 @@
+﻿namespace ChronoWiz;
+
+public partial class FileICS : ContentPage
+{
+	public FileICS()
+	{
+		try
+		{
+			InitializeComponent();
+		}
+		catch (Exception ex)
+		{
+			var tst = ex;
+		}
+	}
+
+#if WINDOWS
+	protected override void OnSizeAllocated(double width, double height)
+	{
+		base.OnSizeAllocated(width, height);
+
+		TotalStack.Scale = 1.0f / TotalStack.Scale;
+
+		double WidthFactor = width / TotalStack.Width;
+		double HeightFactor = height / TotalStack.Height;
+
+		if (WidthFactor < HeightFactor)
+		{
+			TotalStack.Scale = WidthFactor;
+		}
+		else
+		{
+			TotalStack.Scale = HeightFactor;
+		}
+	}
+#endif
+
+	private async void OpenICSButton_Clicked(object sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync
+		(
+			nameof(OpenICS)
+			, true
+		);
+	}
+
+	private async void SaveToICSButton_ClickedAsync(object sender, EventArgs e)
+	{
+		await Shell.Current.GoToAsync
+		(
+			nameof(SaveToICS)
+			, true
+		);
+	}
+}
