6bdf4b86c8182e8016215e5356889887ac0faa4b
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2025-07-07 09:05:42 +0200

    Develop signing

---
+++ macPublish_Ad_Hoc_Develop.sh
@@ -0,0 +1,4 @@
+dotnet build -t:Clean
+dotnet clean
+rm -rf /Users/eks/Downloads/ChronoWiz*.pkg
+dotnet publish ChronoWiz/ChronoWiz.csproj -f net9.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:CreatePackage=true -p:EnableCodeSigning=true  -p:CodesignKey="Apple Development: Created via API (765QB69WBN)" -p:CodesignProvision="Chronowiz_macOS_App_Development" -p:CodesignEntitlements="Platforms/MacCatalyst/Entitlements.Release.plist" -p:UseHardenedRuntime=true -o "/Users/eks/Downloads/"
