d28840eb075213708667a3a778fd63860133f87e
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2025-07-07 09:50:38 +0200

    Update publish commands and output directories
    Modified `dotnet publish` commands in `.bat` and `.sh` files to adjust code signing parameters and output directories. Changed output to `.\Packs` for `.bat` files and `/Users/eks/Downloads/` for `.sh` files. Removed `EnableCodeSigning` in some instances and added `dotnet clean` in `.sh` files. Added comments to indicate previous commands.

--- macPublish_Ad_Hoc_Develop.bat
+++ macPublish_Ad_Hoc_Develop.bat
@@ -1,7 +1,5 @@
 dotnet build -t:Clean
 dotnet clean
-dotnet publish ChronoWiz/ChronoWiz.csproj -f net9.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:CreatePackage=true -p:EnableCodeSigning=true  -p:CodesignKey="Apple Development: Created via API (765QB69WBN)" -p:CodesignProvision="Chronowiz_macOS_App_Development" -p:CodesignEntitlements="Platforms/MacCatalyst/Entitlements.Release.plist" -p:UseHardenedRuntime=true -o ".\Packs"
+dotnet publish ChronoWiz/ChronoWiz.csproj -f net9.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:CreatePackage=true -p:UseHardenedRuntime=true -o ".\Packs"
 
-@REM dotnet build -t:Clean
-@REM dotnet clean
-@REM dotnet publish ChronoWiz\ChronoWiz.csproj -f net9.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:CreatePackage=true -p:EnableCodeSigning=true  -p:CodesignKey="Apple Development: Created via API (765QB69WBN)" -p:CodesignProvision="Chronowiz_macOS_App_Development" -p:CodesignEntitlements="Platforms\MacCatalyst\Entitlements.Release.plist" -p:UseHardenedRuntime=true -o "d:\Users\eigil\Downloads\"
+@REM dotnet publish ChronoWiz/ChronoWiz.csproj -f net9.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:CreatePackage=true -p:EnableCodeSigning=true  -p:CodesignKey="Apple Development: Created via API (765QB69WBN)" -p:CodesignProvision="Chronowiz_macOS_App_Development" -p:CodesignEntitlements="Platforms/MacCatalyst/Entitlements.Release.plist" -p:UseHardenedRuntime=true -o ".\Packs"
--- macPublish_Ad_Hoc_No_Package_Develop.bat
+++ macPublish_Ad_Hoc_No_Package_Develop.bat
@@ -1,3 +1,5 @@
 dotnet build -t:Clean
 dotnet clean
-dotnet publish ChronoWiz\ChronoWiz.csproj -f net9.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:EnableCodeSigning=true  -p:CodesignKey="Apple Development: Created via API (765QB69WBN)" -p:CodesignProvision="Chronowiz_macOS_App_Development" -p:CodesignEntitlements="Platforms\MacCatalyst\Entitlements.Release.plist" -p:UseHardenedRuntime=true
+dotnet publish ChronoWiz\ChronoWiz.csproj -f net9.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:UseHardenedRuntime=true
+
+@REM dotnet publish ChronoWiz\ChronoWiz.csproj -f net9.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:EnableCodeSigning=true  -p:CodesignKey="Apple Development: Created via API (765QB69WBN)" -p:CodesignProvision="Chronowiz_macOS_App_Development" -p:CodesignEntitlements="Platforms\MacCatalyst\Entitlements.Release.plist" -p:UseHardenedRuntime=true
