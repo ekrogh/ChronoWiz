9506e2eb0b5eb1d58f51ec3e1571a1a13f597f7f
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2025-07-10 21:21:46 +0200

    Update code signing provisions in publish scripts
    Added new code signing provision for `Chronowiz_macOS_App_Development` in the second `dotnet publish` command across `macPublish_Ad_Hoc.bat`, `macPublish_Ad_Hoc.sh`, and `macTstPublish_Ad_Hoc.sh`. The first command retains the previous provisioning profile. Updated output directories for published packages to `.\Packs` and `/Users/eks/Downloads/` respectively.

--- macPublish_Ad_Hoc.bat
+++ macPublish_Ad_Hoc.bat
@@ -1,6 +1,6 @@
 dotnet build -t:Clean
 dotnet clean
-dotnet publish ChronoWiz/ChronoWiz.csproj -f net9.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:CreatePackage=true -p:EnableCodeSigning=true  -p:CodesignKey="Apple Development: Eigil Krogh (M39X2ZPF3C)" -p:CodesignProvision="ChronoWiz Dvl MacCatalyst" -p:CodesignEntitlements="Platforms/MacCatalyst/Entitlements.Release.plist" -p:UseHardenedRuntime=true -o ".\Packs"
+dotnet publish ChronoWiz/ChronoWiz.csproj -f net9.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:CreatePackage=true -p:EnableCodeSigning=true  -p:CodesignKey="Apple Development: Eigil Krogh (M39X2ZPF3C)" -p:CodesignProvision="Chronowiz_macOS_App_Development" -p:CodesignEntitlements="Platforms/MacCatalyst/Entitlements.Release.plist" -p:UseHardenedRuntime=true -o ".\Packs"
 
 @REM dotnet build -t:Clean
 @REM dotnet clean
