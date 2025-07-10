13f35558095fd8bdc7b8d99a85fd3301ecffd230
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2025-07-10 19:41:13 +0200

    New certs. and provisions

--- macTstPublish_Ad_Hoc.sh
+++ macTstPublish_Ad_Hoc.sh
@@ -1,3 +1,3 @@
 dotnet build -t:Clean
 dotnet clean
-dotnet publish ChronoWiz/ChronoWiz.csproj -f net9.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:CreatePackage=true -p:EnableCodeSigning=true -p:CodesignKey="Apple Distribution: Eigil Krogh (4657Q2Y6NH)" -p:CodesignProvision="ChronoWiz_Mac_App_Store" -o "/Users/eks/Downloads/"
+dotnet publish ChronoWiz/ChronoWiz.csproj -f net8.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:CreatePackage=true -p:EnableCodeSigning=true -p:CodesignKey="Apple Distribution: Eigil Krogh (4657Q2Y6NH)" -p:CodesignProvision="ChronoWiz Dvl MacCatalyst" -o "/Users/eks/Downloads/"
---
+++ tmp_Scripts/macPublish_App_Store_No_Package.sh
@@ -0,0 +1,3 @@
+dotnet build -t:Clean
+dotnet clean
+dotnet publish ChronoWiz/ChronoWiz.csproj -f net9.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:EnableCodeSigning=true -p:CodesignKey="Apple Distribution: Eigil Krogh (4657Q2Y6NH)" -p:CodesignProvision="ChronoWiz_Mac_App_Store" -p:CodesignEntitlements="Platforms/MacCatalyst/Entitlements.Release.plist"
