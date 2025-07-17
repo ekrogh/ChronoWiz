e4d4d1380f1e4f9180eaaed6b4233c036de1101f
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2025-07-17 16:10:46 +0200

    Revert "scripts"
    This reverts commit 157080c50b13e609b54c71d77e3020b341dd6252.

--- macPublish_Ad_Hoc.sh
+++ macPublish_Ad_Hoc.sh
@@ -1,4 +1,4 @@
 dotnet build -t:Clean
 dotnet clean
 rm -rf /Users/eks/Downloads/ChronoWiz*.pkg
-dotnet publish ChronoWiz/ChronoWiz.csproj -f net9.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:CreatePackage=true -p:EnableCodeSigning=true  -p:CodesignKey="Apple Development: Created via API (K928FRRPW3)" -p:CodesignProvision="Chronowiz_macOS_App_Development" -p:CodesignEntitlements="Platforms/MacCatalyst/Entitlements.Release.plist" -p:UseHardenedRuntime=true -o "/Users/eks/Downloads/"
+dotnet publish ChronoWiz/ChronoWiz.csproj -f net9.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:CreatePackage=true -p:EnableCodeSigning=true  -p:CodesignKey="Apple Development: Eigil Krogh (M39X2ZPF3C)" -p:CodesignProvision="Chronowiz_macOS_App_Development" -p:CodesignEntitlements="Platforms/MacCatalyst/Entitlements.Release.plist" -p:UseHardenedRuntime=true -o "/Users/eks/Downloads/"
