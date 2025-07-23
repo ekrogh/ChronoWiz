21e1a0c0c7997c174db5c4159ae410f24fad536f
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2025-07-23 15:24:30 +0200

    Update project resources and codesigning configurations
    - Added `Compile` and `EmbeddedResource` item groups for `Resources.Designer.cs` and `Resources.resx` in `ChronoWiz.csproj`.
    - Modified `CodesignKey`, `CodesignProvision`, and `ProvisioningType` for `net9.0-ios` to use manual provisioning with a distribution profile.
    - Updated `iosPublish_Ad_Hoc.sh` and `iosPublish_App_Store.sh` scripts to reflect new codesigning provisions.
    - Auto-generated `Resources.Designer.cs` for managing localized resources.
    - Created/updated `Resources.resx` with XML schema for resource management.

--- iosPublish_Ad_Hoc.sh
+++ iosPublish_Ad_Hoc.sh
@@ -1,4 +1,4 @@
 dotnet build -t:Clean
 dotnet clean
 rm -rf /Users/eks/Downloads/ChronoWiz*.*
-dotnet publish ChronoWiz/ChronoWiz.csproj -f net9.0-ios -c Release -p:ArchiveOnBuild=true -p:RuntimeIdentifier=ios-arm64 -p:CodesignKey="Apple Distribution: Eigil Krogh (4657Q2Y6NH)" -p:CodesignProvision="ChronoWiz Ad Hoc Distribution Profile" -o "/Users/eks/Downloads/"
+dotnet publish ChronoWiz/ChronoWiz.csproj -f net9.0-ios -c Release -p:ArchiveOnBuild=true -p:RuntimeIdentifier=ios-arm64 -p:CodesignKey="Apple Distribution: Eigil Krogh (4657Q2Y6NH)" -p:CodesignProvision="ChronoWiz_Ad_Hoc_Distribution_Profile" -o "/Users/eks/Downloads/"
