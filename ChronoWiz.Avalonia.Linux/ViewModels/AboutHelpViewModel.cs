d53310e8c509c600b714e3083c4d87b8f4bd99f8
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2026-02-04 00:12:01 +0100

    Show app name/version in About; add assembly version info
    Updated AboutHelpViewModel to display dynamic app name and version using assembly metadata. Added explicit versioning fields to ChronoWiz.Avalonia.Linux.csproj for consistent build/version info. Reformatted AboutHelpView.axaml for readability and bound AppInfoText to new dynamic values.

--- ChronoWiz.Avalonia.Linux/ViewModels/AboutHelpViewModel.cs
+++ ChronoWiz.Avalonia.Linux/ViewModels/AboutHelpViewModel.cs
@@ -1,40 +1,50 @@
 using ChronoWiz.Avalonia.Linux.Services;
 using ChronoWiz.Shared;
-using System;
+using System.Reflection;
 using System.Windows.Input;
 
 namespace ChronoWiz.Avalonia.Linux.ViewModels;
 
 public sealed class AboutHelpViewModel : ViewModelBase
 {
-	private readonly ILinkOpener _linkOpener;
+    private readonly ILinkOpener _linkOpener;
+    private readonly string _appName;
+    private readonly string _appVersion;
 
-	public string Title => "Help";
+    public string Title => "Help";
 
-	public string AppInfoText => "ChronoWiz\nLinux UI powered by Avalonia";
+    public string AppInfoText => $"{_appName}  Version: {_appVersion}";
 
-	public string Author => "Eigil Krogh Sorensen";
+    public string Author => "Eigil Krogh Sorensen";
 
-	public string Url => Links.HomePage.ToString();
+    public string Url => Links.HomePage.ToString();
 
-	public string Email => "eks@eksit.dk";
+    public string Email => "eks@eksit.dk";
 
-	public ICommand OpenUsersGuideCommand { get; }
-	public ICommand OpenHomePageCommand { get; }
-	public ICommand OpenEmailCommand { get; }
-	public ICommand GoBackCommand { get; }
+    public ICommand OpenUsersGuideCommand { get; }
+    public ICommand OpenHomePageCommand { get; }
+    public ICommand OpenEmailCommand { get; }
+    public ICommand GoBackCommand { get; }
 
-	public AboutHelpViewModel()
-		: this(new ProcessLinkOpener())
-	{
-	}
+    public AboutHelpViewModel()
+        : this(new ProcessLinkOpener())
+    {
+    }
 
-	public AboutHelpViewModel(ILinkOpener linkOpener)
-	{
-		_linkOpener = linkOpener;
-		OpenUsersGuideCommand = new RelayCommand(() => _ = _linkOpener.OpenAsync(Links.UsersGuide));
-		OpenHomePageCommand = new RelayCommand(() => _ = _linkOpener.OpenAsync(Links.HomePage));
-		OpenEmailCommand = new RelayCommand(() => _ = _linkOpener.OpenAsync(Links.MailTo));
-		GoBackCommand = new RelayCommand(() => Navigation?.GoBack(), () => Navigation?.CanGoBack ?? false);
-	}
+    public AboutHelpViewModel(ILinkOpener linkOpener)
+    {
+        _linkOpener = linkOpener;
+
+        var asm = Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly();
+        _appName = asm.GetName().Name ?? "ChronoWiz";
+        //_appVersion =
+        //    asm.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+        //    ?? asm.GetName().Version?.ToString()
+        //    ?? "";
+        _appVersion = Assembly.GetEntryAssembly()?.GetName().Version?.ToString() ?? "";
+        OpenUsersGuideCommand = new RelayCommand(() => _ = _linkOpener.OpenAsync(Links.UsersGuide));
+        OpenHomePageCommand = new RelayCommand(() => _ = _linkOpener.OpenAsync(Links.HomePage));
+        OpenEmailCommand = new RelayCommand(() => _ = _linkOpener.OpenAsync(Links.MailTo));
+        GoBackCommand = new RelayCommand(() => Navigation?.GoBack(), () => Navigation?.CanGoBack ?? false);
+    }
 }
