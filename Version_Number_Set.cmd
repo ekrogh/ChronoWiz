723382e9be1208d7aef27f7a8d4aee17e638d86d
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2026-02-06 01:41:40 +0100

    Ver.: 2.0.1.13

--- Version_Number_Set.cmd
+++ Version_Number_Set.cmd
@@ -1,3 +1,3 @@
-D:\Program_Files\Microsoft_Visual_Studio\2022\Community\Common7\IDE\devenv.exe .\ChronoWiz.sln
+D:\Program_Files\Microsoft_Visual_Studio\18\Community\Common7\IDE\devenv.exe .\ChronoWiz.sln
 call Code.cmd .\snap\snapcraft.yaml .\snap\gui\ChronoWiz.desktop
 D:\Program_Files\SmartGit\bin\smartgit.exe .
