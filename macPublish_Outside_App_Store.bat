f4f86169048617a9da445b37e6f9857f5584a64b
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2026-02-07 00:26:07 +0100

    Update MacCatalyst publish command and output path
    Changed output directory to .\Pack and added --sc option to the ChronoWiz MacCatalyst publish command. Removed old command and retained REM comments for reference.

--- macPublish_Outside_App_Store.bat
+++ macPublish_Outside_App_Store.bat
@@ -1,6 +1,6 @@
 dotnet build -t:Clean
 dotnet clean
-dotnet publish ChronoWiz\ChronoWiz.csproj -f net10.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:CreatePackage=true -p:EnableCodeSigning=true -p:EnablePackageSigning=true -p:CodesignKey="Developer ID Application: Eigil Krogh (4657Q2Y6NH)" -p:CodesignProvision="ChronoWiz_Developer_ID_Application" -p:CodesignEntitlements="Platforms\MacCatalyst\Entitlements.Release.plist" -p:PackageSigningKey="Developer ID Installer: Eigil Krogh (4657Q2Y6NH)" -o "d:\Users\eigil\Downloads\"
+dotnet publish ChronoWiz\ChronoWiz.csproj -f net10.0-maccatalyst -c Release --sc  -o ".\Pack" -p:MtouchLink=SdkOnly -p:CreatePackage=true -p:EnableCodeSigning=true -p:EnablePackageSigning=true -p:CodesignKey="Developer ID Application: Eigil Krogh (4657Q2Y6NH)" -p:CodesignProvision="ChronoWiz_Developer_ID_Application" -p:CodesignEntitlements="Platforms\MacCatalyst\Entitlements.Release.plist" -p:PackageSigningKey="Developer ID Installer: Eigil Krogh (4657Q2Y6NH)"
 
 
 @REM dotnet publish ChronoWiz/ChronoWiz.csproj -f net10.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:CreatePackage=true -p:EnableCodeSigning=true -p:EnablePackageSigning=true -p:CodesignKey="Developer ID Application: Eigil Krogh (4657Q2Y6NH)" -p:CodesignProvision="ChronoWiz_Developer_ID_Application" -p:CodesignEntitlements="Platforms/MacCatalyst/Entitlements.Release.plist" -p:PackageSigningKey="Developer ID Installer: Eigil Krogh (4657Q2Y6NH)" -o "/Users/eks/Downloads/"
