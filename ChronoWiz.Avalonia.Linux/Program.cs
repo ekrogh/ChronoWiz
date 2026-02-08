30f5d55a5b10d9a5820b9b881c8321596066294c
Eigil Krogh Sorensen <eigilkrogh@icloud.com>
2026-02-08 06:35:05 +0100

    Disable GPU, force Skia/X11 for Avalonia on Linux
    Set AVALONIA_DISABLE_GPU to disable GPU rendering. Updated app
    builder to use X11 and Skia explicitly, avoiding GLX/OpenGL for
    better Linux compatibility. Added necessary using directives.

--- ChronoWiz.Avalonia.Linux/Program.cs
+++ ChronoWiz.Avalonia.Linux/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Skia;
 using System;
 
 namespace ChronoWiz.Avalonia.Linux;
@@ -9,13 +11,17 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        Environment.SetEnvironmentVariable("AVALONIA_DISABLE_GPU", "1");
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
-            .UsePlatformDetect()
+            .UseX11()
+            .UseSkia() // forces Skia renderer; avoids GLX/OpenGL
             .WithInterFont()
             .LogToTrace();
 }
