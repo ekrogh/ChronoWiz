d00daec048519a781c1faba0f3115fbfad09129f
eigil Krogh <eigilkrogh@icloud.com>
2026-02-17 09:02:28 +0100

    Update publish command to include ArchiveOnBuild option for Mac Catalyst

--- macPublish_App_Store.sh
+++ macPublish_App_Store.sh
@@ -79,4 +79,4 @@ echo "Using provisioning profile '$PROFILE_NAME' (UUID: $PROFILE_UUID)"
 dotnet build -t:Clean
 dotnet clean
 rm -rf /Users/eks/Downloads/ChronoWiz*.pkg
-dotnet publish ChronoWiz/ChronoWiz.csproj -f net10.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:CreatePackage=true -p:EnableCodeSigning=true -p:EnablePackageSigning=true -p:CodesignKey="Apple Distribution: Eigil Krogh (4657Q2Y6NH)" -p:CodesignProvision="$PROFILE_UUID" -p:CodesignEntitlements="Platforms/MacCatalyst/Entitlements.Release.plist" -p:MtouchExtraArgs="--nowarn:7151" -p:PackageSigningKey="3rd Party Mac Developer Installer: Eigil Krogh (4657Q2Y6NH)" -o "/Users/eks/Downloads/"
+dotnet publish ChronoWiz/ChronoWiz.csproj -f net10.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:ArchiveOnBuild=true -p:CreatePackage=true -p:EnableCodeSigning=true -p:EnablePackageSigning=true -p:CodesignKey="Apple Distribution: Eigil Krogh (4657Q2Y6NH)" -p:CodesignProvision="$PROFILE_UUID" -p:CodesignEntitlements="Platforms/MacCatalyst/Entitlements.Release.plist" -p:MtouchExtraArgs="--nowarn:7151" -p:PackageSigningKey="3rd Party Mac Developer Installer: Eigil Krogh (4657Q2Y6NH)" -o "/Users/eks/Downloads/"
