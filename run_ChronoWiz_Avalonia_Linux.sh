56658aa76421be0d082843fb25b19b69ad8fe7a6
Eigil Krogh <eigilkrogh@icloud.com>
2026-03-01 17:32:35 +0100

    update

--- run_ChronoWiz_Avalonia_Linux.sh
+++ run_ChronoWiz_Avalonia_Linux.sh
@@ -1,2 +1,2 @@
 # dotnet clean ChronoWiz.Avalonia.Linux/ChronoWiz.Avalonia.Linux.csproj -c Release -o ./built
-dotnet run --project ChronoWiz.Avalonia.Linux/ChronoWiz.Avalonia.Linux.csproj -c Release -o ./built
+dotnet run --no-launch-profile --project ChronoWiz.Avalonia.Linux/ChronoWiz.Avalonia.Linux.csproj -c Release -o ./built
